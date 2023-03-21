610afba6e240896056d3068cd9b57a2901f39aed
HaaaamBurger <gemasclashes@gmail.com>
2023-03-21 12:07:34 +0200

    #

--- hello.js
+++ hello.js
@@ -9,3 +9,15 @@
 //
 // console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
 //----------------------------------------------------------------------------------------------------------------------
+// https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
+//----------------------------------------------------------------------------------------------------------------------
+// function uniqueInOrder(iterable) {
+//     let newArr = [];
+    // for (let i = 0; i < iterable.length; i++) {
+    //     if (iterable[i] !== iterable[i + 1]) newArr.push(iterable[i]);
+    // }
+    // return newArr;
+}
+// console.log(uniqueInOrder('AAAABBBCCDAABBB'));
+//uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+//----------------------------------------------------------------------------------------------------------------------
